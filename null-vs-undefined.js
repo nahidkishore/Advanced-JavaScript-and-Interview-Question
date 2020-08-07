46c56bbe60c450aa24377c3937cfa25880f5131b
nahid <nahidkishore99@gmail.com>
2020-08-07 11:38:26 +0600

    null vs undefined cleared for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,13 @@
+let moynaPakhi;
+
+ function add(num1, num2){
+  console.log(num1+num2);
+  return
+} 
+
+ function add(num1, num2){
+  console.log(num1,num2)
+} 
+const result=add(12);
+const premikPurus={name:'solaiman Khan', phone:'8888389' };
+console.log(premikPurus.gf);
