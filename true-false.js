22abc2a3dea3a5e43e85a5c4b1b203b4f9da164d
nahid <nahidkishore99@gmail.com>
2020-08-07 10:52:19 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,19 @@
+//false condition gulo.....
+// 0
+// ""
+// undefined
+// null
+// NaN
+// false
+
+
+// true condition
+// "0", " ",[], {}, jekono kicho
+//const age=45;
+const name="nahid";
+if(name){
+  console.log('condition is true');
+}
+else{
+  console.log('condition is false');
+}
