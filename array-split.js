ddcf1b9cf09a7ec298998689f5da22860635e6ae
nahid <nahidkishore99@gmail.com>
2020-08-07 21:54:42 +0600

    sliice, splice, join array

---
+++ array-split.js
@@ -0,0 +1,12 @@
+const nums=[2,4,5,6,7,40,41,22,32];
+const part =nums.slice(2,6);
+console.log(part);
+console.log(nums);
+const remove=nums.splice(2,5);
+console.log(remove);
+
+
+console.log(nums);
+
+const together=nums.join(" next ");
+console.log(together);
